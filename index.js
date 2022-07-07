4107d2adf8677fe6de04cf5bc2839f1ad13801f6
CodeJunkie01 <p2h4go@gmail.com>
2022-07-07 22:45:36 +0200

    x

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const shell = require('shelljs')
 var express = require('express')
 var app = express()
 var fs = require('fs')
+const bodyParser = require("body-parser");
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 app.listen(3001, () => {
  console.log("Server running on port 3000");
@@ -15,6 +20,7 @@ app.get('/update', (req,res) => {
 
 app.post('/newApp', (req,res)=> {
     
+        console.log(req.body);
         const {newProfile} = req.body;
     
         
