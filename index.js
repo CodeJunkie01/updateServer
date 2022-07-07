d368e5991067d081f4d7acd7a9272fbd06f57729
CodeJunkie01 <p2h4go@gmail.com>
2022-07-07 21:35:29 +0200

    added ability to push new config via http

--- index.js
+++ index.js
@@ -11,3 +11,24 @@ app.get('/update', (req,res) => {
  shell.exec('./update.sh');
  res.json(["SUCCESS"]);
 })
+
+app.get('/newApp', (req,res)=> {
+    if(req.body.newProfile){
+        const newProfile = req.body.newProfile;
+    } else {
+        console.log("no Profile found");
+        return res.json("no Profile found");
+    }
+    
+    fs.readFile('../prod/appConfigs.js', 'utf-8', function(err, data){
+        if (err) throw err;
+    
+        var newValue = data + " " + newProfile;
+    
+        fs.writeFile('../prod/appConfigs.js', newValue, 'utf-8', function (err) {
+          if (err) throw err;
+          console.log(newValue);
+          return res.json("success");
+        });
+      });
+})
